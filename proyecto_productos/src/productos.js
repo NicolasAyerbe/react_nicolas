2bcbebef865db67b5962b8702f61473e25098e04
trujillo777 <trujilloandrea965@gmail.com>
2022-08-22 11:19:24 -0500

    version 1.2

--- proyecto_productos/src/productos.js
+++ proyecto_productos/src/productos.js
@@ -1,53 +1,87 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import './productos.css';
-const listarproductos = [
 
-    {
-        id: 1,
-        descripcion: "Zapatito rojo",
-        precio:12000,
-    },
-    {
-        id: 2,
-        descripcion: "zapatito azul",
-        precio:14000,
-    }
+const list = [
+	{
+		id:1,
+		nombre:"david",
+		year:1967,
+	},
+	{
+		id:2,
+		nombre:"nicolas",
+		year:1985,
+	},
+	{
+		id:3,
+		nombre:"juan",
+		year:1981,
+	}
+];
+//46..<div key={item.id}>   70 </div>
+class ProductosII extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			list,
+		};
 
-]
+		this.Eliminar = 
+		this.Eliminar.bind(this);
+	}
+	
+	Eliminar(id) {
+		const isNotId = item => item.id !== id;
+		const listaActualizada = 
+		this.state.list.filter(isNotId);
+		this.setState({ list: listaActualizada });
+	}
+   render() {
+      return(
+        <div><h1>listado </h1> 
+		<div className='principal'>
+           
+            
+             <table>
+                <thead>
+                     <tr>
+                        <th>Nombre</th><th>Año</th><th>delete</th>
+                        </tr>
+                </thead>
+			{this.state.list.map(item => 
+				
+                   
+					<tbody>
+                       
+                        <tr>
 
-class productos extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            listarproductos: listarproductos,
-        }
-    }
-
-    render(){
-        return (
-            <div className="principal">
-                <div><h2>Listado</h2>
-
-                <table>
+                            <td>
+						{item.nombre} 
+						</td>
+                        <td>
+						{item.year} 
+						</td>
+                        <td>
+						<button class="cursor-style" 
+							onClick={() => this.Eliminar(item.id)}
+							type="button"
+						>
+						Eliminar
+						</button> 
+                        </td>
+					
+                    </tr>
+                    </tbody>
                     
-                    <th>Nombre</th><th>Precio</th><th></th>
-                    
-                    
-                    
-            {
-                listarproductos.map(function(item) {
-                    return  <tr><td>{item.descripcion}</td><td>{item.precio}</td><td ><button class="cursor-style">Eliminar</button></td></tr>
-                    
-                  
-                    
-                })
-            }
-      
+				
+			)}
+            
             </table>
-            </div>
-            </div>
-        
-        )
-    }
+		</div>
+        </div>
+      );
+   }
 }
-export default productos;
+
+export default ProductosII;
